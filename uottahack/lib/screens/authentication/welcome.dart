43ecf70f5d68e3688bdc6b2971ad2bf65001ede9
Seneli Seneviratne <seneliseneviratne@cmail.carleton.ca>
2021-02-07 08:44:09 -0500

    rough styling for welcome page

--- uottahack/lib/screens/authentication/welcome.dart
+++ uottahack/lib/screens/authentication/welcome.dart
@@ -10,22 +10,50 @@ class Welcome extends StatefulWidget {
 class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
-      children: [
-        RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => SignIn()));
-            },
-            child: Text("Sign In")),
-        RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Register()));
-            },
-            child: Text("Sign Up"))
-      ],
-    ));
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Check In",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 50.0,
+                  //fontFamily: c
+              )),
+          Image.asset('assets/images/timbit.png',
+              height: 500,
+              width: 500
+              ),
+          Container(
+            child: Row(
+              children: [
+                RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => SignIn()));
+                    },
+                    padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.width * 0.10,
+                      horizontal: MediaQuery.of(context).size.width * 0.15,
+                    ),
+                    child: Text("Sign In")),
+                Spacer(),
+                RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => Register()));
+                    },
+                    padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.width * 0.10,
+                      horizontal: MediaQuery.of(context).size.width * 0.15,
+                    ),
+                    child: Text("Sign Up"))
+              ],
+            ),
+          ),
+        ],
+
+      ),
+    );
   }
 }
